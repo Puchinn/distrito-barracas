bb55e39a711f22954af52fb884b41f68967b5bd3
Esteban Sayago <sayagoo823@gmail.com>
2026-01-12 09:30:42 -0300

    fix: pages images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+
+  images: {
+    unoptimized: true,
+  },
+
+  basePath: "/distrito-barracas",
+  assetPrefix: "/distrito-barracas/",
 };
 
 export default nextConfig;
